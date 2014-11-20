c28578db1c4e819ec804af783d44c307d0b94b5c
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-11-21 01:54:15 +0200

    update bower.json

--- dist/formus.js
+++ dist/formus.js
@@ -41,6 +41,13 @@
               default: false
             };
           },
+          checklist: function () {
+            return {
+              trueValue: true,
+              falseValue: false,
+              items: []
+            };
+          },
           radio: function () {
             return { inline: true };
           },
@@ -752,6 +759,7 @@ angular.module('formus').run([
     $templateCache.put('formus/form.html', '<form role=form id={{name}} class={{config.class}} style={{config.style}} ng-submit=submit()><header><div ng-if="config.showErrors && !isValid" class="alert alert-danger"><ul><li ng-repeat="e in errorList" ng-bind=e></li></ul></div></header><formus-field config=fieldset class=special></formus-field><div class=clear-fix></div><footer><div ng-repeat="btn in config.buttons" class=pull-left><button class={{btn.class}} type=button ng-if=!btn.items ng-click=btn.handler()>{{btn.title}}</button><div class="btn-group margin-left-5" ng-if=btn.items><button class="{{btn.class}} dropdown-toggle" type=button data-toggle=dropdown>{{btn.title}} <span class=caret></span></button><ul class=dropdown-menu><li ng-repeat="item in btn.items"><a ng-click=item.handler()>{{item.title}}</a></li></ul></div></div><button ng-if=config.submit type=submit class={{config.submit.class}} ng-bind=config.submit.title></button></footer></form>');
     $templateCache.put('formus/inputs/button.html', '<button type=button class={{config.class}} ng-bind=config.title ng-click=config.handler()></button>');
     $templateCache.put('formus/inputs/checkbox.html', '<div class=checkbox><label><input type=checkbox ng-true-value={{config.trueValue}} ng-false-value={{config.falseValue}} ng-model=model name={{config.name}}>{{config.label}}</label></div>');
+    $templateCache.put('formus/inputs/checklist.html', '<div ng-if=!config.inline><div class=checkbox ng-repeat="item in config.items"><label><input ng-true-value={{config.trueValue}} ng-false-value={{config.falseValue}} name={{name}} type=checkbox ng-model=$parent.$parent.model[item.value]>{{item.title}}</label></div></div><div ng-if=config.inline><label class=checkbox-inline ng-repeat="item in config.items"><input ng-true-value={{config.trueValue}} ng-false-value={{config.falseValue}} name={{name}} type=checkbox ng-model=$parent.$parent.model[item.value]>{{item.title}}</label></div>');
     $templateCache.put('formus/inputs/fieldset.html', '<div class={{config.class}}><formus-wrapper ng-repeat="field in config.fields" class={{config.wrapClass}}><formus-field config=field></formus-field></formus-wrapper></div>');
     $templateCache.put('formus/inputs/radio.html', '<div ng-if=!config.inline><div class=radio ng-repeat="item in config.items"><label><input ng-value=item.value name={{name}} type=radio ng-model=$parent.$parent.model>{{item.title}}</label></div></div><div ng-if=config.inline><label class=radio-inline ng-repeat="item in config.items"><input ng-value=item.value name={{name}} type=radio ng-model=$parent.$parent.model>{{item.title}}</label></div>');
     $templateCache.put('formus/inputs/select.html', '<select name={{config.name}} ng-model=model class=form-control ng-multiple=config.multiple style={{config.style}} id={{config.name}} ng-options="item.value as item.title for item in config.items"><option value ng-if=config.empty>{{config.empty}}</option></select>');
